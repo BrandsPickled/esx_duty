afd87fcc9416c7c2c6b22a43e9b13f4f962c26db
BrandsPickled <brandonvjcooley@gmail.com>
2019-05-29 01:16:18 +0100

    Update jobs.sql

--- jobs.sql
+++ jobs.sql
@@ -1,7 +1,8 @@
 INSERT INTO `jobs` (name, label) VALUES
   ('offpolice','Off-Duty'),
-  ('offmechanic','Off-Duty'),
-  ('offambulance','Off-Duty')
+  ('offambulance','Off-Duty'),
+  ('offtaxi','Off-Duty'), 
+  ('offmechanic','Off-Duty')
 ;
 
 INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
@@ -10,13 +11,18 @@ INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_
   ('offpolice',2,'sergeant','Sergeant',36,'{}','{}'),
   ('offpolice',3,'lieutenant','Lieutenant',48,'{}','{}'),
   ('offpolice',4,'boss','Boss',0,'{}','{}'),
-  ('offmechanic',0,'recrue','Recruit',12,'{}','{}'),
-  ('offmechanic',1,'novice','Novice',24,'{}','{}'),
-  ('offmechanic',2,'experienced','Experienced',36,'{}','{}'),
-  ('offmechanic',3,'chief','Chief',48,'{}','{}'),
-  ('offmechanic',4,'boss','Boss',0,'{}','{}'),
   ('offambulance',0,'ambulance','Ambulance',12,'{}','{}'),
   ('offambulance',1,'doctor','Doctor',24,'{}','{}'),
   ('offambulance',2,'chief_doctor','Chef',36,'{}','{}'),
-  ('offambulance',3,'boss','Boss',48,'{}','{}')
+  ('offambulance',3,'boss','Boss',48,'{}','{}'),
+  ('offmechanic',0,'recrue','Recruit',12,'{}','{}'),
+  ('offmechanic',1,'novice','Novice',24,'{}','{}'),
+  ('offmechanic',2,'experienced','Experienced',36,'{}','{}'),
+  ('offmechanic',3,'chief','Leader',36,'{}','{}'),
+  ('offmechanic',4,'boss','Boss',48,'{}','{}'),
+  ('offtaxi',0,'recrue','Recruit',12,'{}','{}'),
+  ('offtaxi',1,'novice','Novice',24,'{}','{}'),
+  ('offtaxi',2,'experimente','Expert',36,'{}','{}'),
+  ('offtaxi',3,'uber','Uber',36,'{}','{}'),
+  ('offtaxi',4,'boss','Boss',48,'{}','{}')
 ;
